12d820ddb4b427a2ec35131759e67ec7d449f1da
unknown <ckcr20@gmail.com>
2024-11-14 13:21:11 +0800

    Refactored File System

--- Mini_Project_1.X/build/default/production/mini_project_1.o.d
+++ Mini_Project_1.X/build/default/production/mini_project_1.o.d
@@ -1,1 +1,10 @@
-build/default/production/mini_project_1.o: mini_project_1.c
+build/default/production/mini_project_1.o: mini_project_1.c eic.h \
+ init_io.h pin_calculation.h clk.h
+
+eic.h:
+
+init_io.h:
+
+pin_calculation.h:
+
+clk.h:
---
+++ Mini_Project_1.X/clk.h
@@ -0,0 +1,73 @@
+#ifndef CLK_H
+#define CLK_H
+
+#define INIT_TOP 468 // Let x be the PER value, $100=\frac{48e6}{1024(x+1)}$
+
+void GCLK_Init(void){
+    /* Initialize Clocks */
+    // Main Clock enabled by default
+    GCLK_REGS -> GCLK_PCHCTRL[25] = 0x00000040;
+    while ((GCLK_REGS -> GCLK_PCHCTRL [25] & 0x00000040) == 0) ; // Wait for synchronization
+    }
+ 
+void TCC3_Init(int brightness) {
+    /* Reset TCC */
+    TCC3_REGS->TCC_CTRLA = 0x01; // Set SWRST bit to 1 to reset
+    while(TCC3_REGS->TCC_SYNCBUSY & ~(1<<0)); // Wait for synchronization
+    
+    /* Clock Prescaler and Mode */    
+    TCC3_REGS->TCC_CTRLA = (1 << 12) | (7 << 8); // Precsync = PRESC | Prescaler = 1024
+    
+    TCC3_REGS->TCC_WEXCTRL = TCC_WEXCTRL_OTMX(0UL); // Default configuration
+    TCC3_REGS->TCC_WAVE = (2 << 0) | (0 << 4) | (1<<17);  // 0x2 NPWM Normal PWM PER TOP/Zero or Single slope PWM
+                                                // RAMP 1 operation (Polarity 1) - bit 16, set at CCx, clear at TOP 
+    
+    /* Configure duty cycle values */    
+    TCC3_REGS->TCC_PER = INIT_TOP; // Set the period value
+    TCC3_REGS->TCC_CC[1] = brightness; // Set the capture / compare register 0 or counter value       
+    
+    /* TCC enable */
+    TCC3_REGS->TCC_CTRLA |= (1 << 1); // Enables TCC
+    while(TCC3_REGS->TCC_SYNCBUSY & ~(1<<1)); // Wait for synchronization
+    
+}
+ 
+void Clock_Source(void){
+    
+    /*
+     * The chip starts in PL0 ("Energy Saving"); as a result, the chip is also
+     * limited in the maximum frequency it can support (12 vs 48 MHz). Switch
+     * it to PL2 ("Performance") before doing other Clock_Source() tasks.
+     * 
+     * - Berts
+     */
+    PM_REGS->PM_INTFLAG = 0x01;
+    PM_REGS->PM_PLCFG = 0x02;
+    while ((PM_REGS->PM_INTFLAG & 0x01) == 0)
+        asm("nop");
+    PM_REGS->PM_INTFLAG = 0x01;
+    
+    /****************** Controls Initialization  *********************/    
+    NVMCTRL_SEC_REGS->NVMCTRL_CTRLB = (2<<1);    
+    SUPC_REGS->SUPC_VREGPLL = (1 << 1);
+    
+    /****************** DFLL Initialization  *********************/
+    OSCCTRL_REGS->OSCCTRL_DFLLCTRL = 0;
+    while((OSCCTRL_REGS->OSCCTRL_STATUS & (1<<24)) != (1<<24));  /* Waiting for the Ready state */
+ 
+    /*Load Calibration Value*/
+    uint8_t calibCoarse = (uint8_t)((((uint32_t)0x00806020U) >> 25U ) & 0x3fU);
+    OSCCTRL_REGS->OSCCTRL_DFLLVAL = OSCCTRL_DFLLVAL_COARSE((uint32_t)calibCoarse) | OSCCTRL_DFLLVAL_FINE((uint32_t)512U);
+    while((OSCCTRL_REGS->OSCCTRL_STATUS & (1<<24)) != (1<<24)); /* Waiting for the Ready state */
+ 
+    /* Configure DFLL    */
+    OSCCTRL_REGS->OSCCTRL_DFLLCTRL = (1<<1) ;
+    while((OSCCTRL_REGS->OSCCTRL_STATUS & (1<<24)) != (1<<24)); /* Waiting for DFLL to be ready */
+    
+    /****************** GCLK 0 Initialization  *********************/    
+    GCLK_REGS->GCLK_GENCTRL[0] = (1<<16) | (7<<0) | (1<<8);
+    //while(GCLK_REGS->GCLK_SYNCBUSY & ~(1<<2));
+    while(GCLK_REGS->GCLK_SYNCBUSY & (1<<2))
+        asm("nop");
+}
+#endif
