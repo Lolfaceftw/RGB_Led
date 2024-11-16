fd98f2aab9dd150ef898c1155d684efc0ad31889
Christian Klein C. Ramos <ckcr20@gmail.com>
2024-11-16 14:05:33 +0800

    fixed init, bitwise or

--- Mini_Project_1.X/init_io.h
+++ Mini_Project_1.X/init_io.h
@@ -52,8 +52,8 @@ void PB_02_Init(void){
      *  Group 1: Address Spacing 0x80
      *  Target: PMUX Enabled; ADC Selected
      */
-    PORT_SEC_REGS -> GROUP[1].PORT_PINCFG[1] = 0x1U; // PMUX EN
-    PORT_SEC_REGS -> GROUP[1].PORT_PMUX[1] = 0x1U; // Peripheral B
+    PORT_SEC_REGS -> GROUP[1].PORT_PINCFG[2] |= 0x1; // PMUX EN
+    PORT_SEC_REGS -> GROUP[1].PORT_PMUX[1] |= (0x1 << 0); // Peripheral B
 }
 
 void PA_03_Init(void) {    
@@ -65,13 +65,13 @@ void PA_03_Init(void) {
      *  DIR: 1, INEN: 1, PULLEN: X, OUT: X
      */
     // 31.7.1
-    PORT_SEC_REGS->GROUP[0].PORT_DIRSET = (1 << 3); // Set as output
+    PORT_SEC_REGS->GROUP[0].PORT_DIRSET |= (1 << 3); // Set as output
     // 31.7.6
-    PORT_SEC_REGS->GROUP[0].PORT_OUTCLR = (1 << 3); // Reset Pin
+    PORT_SEC_REGS->GROUP[0].PORT_OUTCLR |= (1 << 3); // Reset Pin
     // 31.7.14
-    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[3] = 0x3; // Enables INEN and PMUXEN, output with input enabled.
+    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[3] |= 0x3; // Enables INEN and PMUXEN, output with input enabled.
     // 31.7.13
-    PORT_SEC_REGS->GROUP[0].PORT_PMUX[1] = (0x90 << 4); // J Peripheral for PA03, PMUXO[3:0], required PMUXEN 1
+    PORT_SEC_REGS->GROUP[0].PORT_PMUX[1] |= (0x9 << 4); // J Peripheral for PA03, PMUXO[3:0], required PMUXEN 1
 }
 
 void PA_06_Init(void) {    
@@ -82,14 +82,14 @@ void PA_06_Init(void) {
      *  Target: Output; input enabled.
      *  DIR: 1, INEN: 1, PULLEN: X, OUT: X
      */
-    // 31.7.1
-    PORT_SEC_REGS->GROUP[0].PORT_DIRSET = (1 << 6); // Set as output
+    // 31.7.1nagl
+    PORT_SEC_REGS->GROUP[0].PORT_DIRSET |= (1 << 6); // Set as output
     // 31.7.0
-    PORT_SEC_REGS->GROUP[0].PORT_OUTCLR = (1 << 6); // Reset Pin
+    PORT_SEC_REGS->GROUP[0].PORT_OUTCLR |= (1 << 6); // Reset Pin
     // 31.7.14
-    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[6] = 0x3; // Enables INEN and PMUXEN, output with input enabled.
+    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[6] |= 0x3; // Enables INEN and PMUXEN, output with input enabled.
     // 31.7.13
-    PORT_SEC_REGS->GROUP[0].PORT_PMUX[1] = (0x90 << 0); // J Peripheral for PA06, PMUXE[3:0], required PMUXEN 1
+    PORT_SEC_REGS->GROUP[0].PORT_PMUX[3] |= (0x9 << 0); // J Peripheral for PA06, PMUXE[3:0], required PMUXEN 1
 }
 
 void PB_03_Init(void) {    
@@ -101,13 +101,13 @@ void PB_03_Init(void) {
      *  DIR: 1, INEN: 1, PULLEN: X, OUT: X
      */
     // 31.7.1
-    PORT_SEC_REGS->GROUP[1].PORT_DIRSET = (1 << 3); // Set as output
+    PORT_SEC_REGS->GROUP[1].PORT_DIRSET |= (1 << 3); // Set as output
     // 31.7.6
-    PORT_SEC_REGS->GROUP[1].PORT_OUTCLR = (1 << 3); // Reset Pin, set to low output drive level.
+    PORT_SEC_REGS->GROUP[1].PORT_OUTCLR |= (1 << 3); // Reset Pin, set to low output drive level.
     // 31.7.14
-    PORT_SEC_REGS->GROUP[1].PORT_PINCFG[3] = 0x3; // Enables INEN and PMUXEN, output with input enabled.
+    PORT_SEC_REGS->GROUP[1].PORT_PINCFG[3] |= 0x3; // Enables INEN and PMUXEN, output with input enabled.
     // 31.7.13
-    PORT_SEC_REGS->GROUP[1].PORT_PMUX[1] = (0x90 << 4); // J Peripheral for PB03, PMUXO[3:0], required PMUXEN 1
+    PORT_SEC_REGS->GROUP[1].PORT_PMUX[1] |= (0x9 << 4); // J Peripheral for PB03, PMUXO[3:0], required PMUXEN 1
 }
 
 
@@ -119,13 +119,13 @@ void SW_1_Init(void){
      *  DIR: 0, INEN: 1, PULLEN: 1, OUT: 1
      */
     // 31.7.1
-    PORT_SEC_REGS->GROUP[0].PORT_DIRSET = (0 << 0); // Set as input.
+    PORT_SEC_REGS->GROUP[0].PORT_DIRSET |= (0 << 0); // Set as input.
     // 31.7.14
-    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[0] = 0x7; // Enables PULLEN, INEN, and PMUXEN, input with pull.
+    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[0] |= 0x7; // Enables PULLEN, INEN, and PMUXEN, input with pull.
     // 31.7.6
-    PORT_SEC_REGS->GROUP[0].PORT_OUTSET= (1 << 0); // Set as internal pull-up.
+    PORT_SEC_REGS->GROUP[0].PORT_OUTSET |= (1 << 0); // Set as internal pull-up.
     // 31.7.13
-    PORT_SEC_REGS->GROUP[0].PORT_PMUX[0] = (0x0 << 0); // A Peripheral for PA00, PMUXE[3:0], required PMUXEN 1
+    PORT_SEC_REGS->GROUP[0].PORT_PMUX[0] |= (0x0 << 0); // A Peripheral for PA00, PMUXE[3:0], required PMUXEN 1
 }
 
 void SW_2_Init(void){
@@ -136,13 +136,13 @@ void SW_2_Init(void){
      *  DIR: 0, INEN: 1, PULLEN: 1, OUT: 1
      */
     // 31.7.1
-    PORT_SEC_REGS->GROUP[0].PORT_DIRSET = (0 << 1); // Set as input.
+    PORT_SEC_REGS->GROUP[0].PORT_DIRSET |= (0 << 1); // Set as input.
     // 31.7.14
-    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[1] = 0x7; // Enables PULLEN, INEN, and PMUXEN, input with pull.
+    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[1] |= 0x7; // Enables PULLEN, INEN, and PMUXEN, input with pull.
     // 31.7.6
-    PORT_SEC_REGS->GROUP[0].PORT_OUTSET= (1 << 1); // Set as internal pull-up.
+    PORT_SEC_REGS->GROUP[0].PORT_OUTSET |= (1 << 1); // Set as internal pull-up.
     // 31.7.13
-    PORT_SEC_REGS->GROUP[0].PORT_PMUX[0] = (0x0 << 4); // A Peripheral for PA01, PMUXO[3:0], required PMUXEN 1
+    PORT_SEC_REGS->GROUP[0].PORT_PMUX[0] |= (0x0 << 4); // A Peripheral for PA01, PMUXO[3:0], required PMUXEN 1
 }
 
 #endif
