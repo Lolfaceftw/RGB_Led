8fe7bea9c5dda581cba7de7bcedac196b3e63dca
Christian Klein C. Ramos <ckcr20@gmail.com>
2024-11-17 09:43:14 +0800

    TC0Init

--- Mini_Project_1.X/clk.h
+++ Mini_Project_1.X/clk.h
@@ -76,4 +76,28 @@ void Clock_Source(void){
     GCLK_REGS -> GCLK_PCHCTRL[28] = (0 << 0) | (1 << 6);
     while((GCLK_REGS -> GCLK_PCHCTRL[28] & (1 << 6)) != (1 << 6));
 }
+
+void TC0_Init(void){
+    // Enable the TC0 Bus Clock
+    GCLK_REGS -> GCLK_PCHCTRL[23] |= (1 << 6); // Bit 6 Enable
+    while ((GCLK_REGS -> GCLK_PCHCTRL [23] * (1 << 6)) == 0);
+    
+    // Setting up the TC 0 -> CTRLA Register
+    TC0_REGS -> COUNT16.TC_CTRLA |= (1); // Software Reset; Bit 0
+    while(TC0_REGS -> COUNT16.TC_SYNCBUSY & (1));
+    
+    TC0_REGS -> COUNT16.TC_CTRLA |= (0x0 << 2); // Set to 16 bit mode; Bit[3:2].
+    TC0_REGS -> COUNT16.TC_CTRLA |= (0x1 << 4); // Reset counter on next prescaler clock Bit[5:4]]
+    TC0_REGS -> COUNT16.TC_CTRLA |= (0x7 << 8); // Prescaler Factor: 1024 Bit[10:8]]
+    
+    // Setting up the WAVE Register
+    TC0_REGS -> COUNT16.TC_WAVE |= (0x1 << 0); // Use MFRQ Bit [1:0]
+    
+    // Setting the Top Value
+    TC0_REGS -> COUNT16.TC_CC[0] |= (0xB71B); // Set CC0 Top Value = 48e6/1024 = 46875 (1s Period)
+  
+    
+    TC0_REGS -> COUNT16.TC_CTRLA |= (1 << 1); // Enable TC0 Peripheral Bit 1
+ 
+}
 #endif
